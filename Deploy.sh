bb8b6f551e8527016d7b8ceb61ec317e8432022c
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-09 16:23:30 +0100

    Keep developing

--- Deploy.sh
+++ Deploy.sh
@@ -24,7 +24,7 @@ do
 	echo "Cloning "${i}"..."
 	git clone --recursive git@github.com:a-helix/${i}.git
 	echo "Done."
-	cd WeatherSubscribeService
+	cd ${i}
 	bash build.sh
 	echo "Copying "${i}" binaries..."
 	cd ..
